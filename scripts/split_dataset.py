266040c5d1b42b9ec869af3eb321a1082bebf2f9
Awwsomes <awwsomes@icloud.com>
2026-03-18 21:31:16 +0800

    Update split_dataset.py
    260318

--- scripts/split_dataset.py
+++ scripts/split_dataset.py
@@ -75,7 +75,7 @@ def split_data(file_path, new_file_path, train_rate, val_rate, test_rate):
         shutil.copy(old_xmlpath, new_xmlpath)
 
 if __name__ == '__main__':
-    file_path = r"D:\A_myData\dataset\corner3\images_no_blank" # 这里放你存JPG照片的地址
-    txtpath = r"D:\A_myData\dataset\corner3\labels" # 这里放你存label的地址
-    new_file_path = r"D:\A_myData\dataset\corner4"  # 这里填你打算新建立的文件夹的名字
+    file_path = r"D:\A_myData\RC26-Vision\dataset\corner4\images" # 这里放你存JPG照片的地址
+    txtpath = r"D:\A_myData\RC26-Vision\dataset\corner4\txts" # 这里放你存label的地址
+    new_file_path = r"D:\A_myData\RC26-Vision\dataset\corner5"  # 这里填你打算新建立的文件夹的名字
     split_data(file_path, new_file_path, train_rate=0.85, val_rate=0.15, test_rate=0) #train，val and test的划分。
