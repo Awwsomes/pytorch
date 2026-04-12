97369ea0575d45329fd57d995d4ee4a8026ab804
Awwsomes <awwsomes@icloud.com>
2026-04-12 16:26:22 +0800

    新增脚本：平衡分类数据集各类别的数量

---
+++ scripts/balance_dataset_every_label_amount_cls.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+import random
+
+from countLabel_cls import count_label_cls
+from rename_data import copy_dirs
+
+def balance_amount(root_path:str) -> dict[str, list]:
+    # 获取最小值
+    label_amount,min_label_amount,_,_,_,_ = count_label_cls(root_path)
+    # 遍历类别
+    output_list = {}
+    for label in label_amount:
+        # 确定每个类别拷贝的数量
+        origin_amount = label_amount[label]
+        print(f"o:{origin_amount}")
+        if origin_amount <= min_label_amount + 20:
+            need_amount = origin_amount
+        else:
+            need_amount = min_label_amount + 20
+        print(f"n: {need_amount}")
+        # 打乱列表,输出
+        label_path = os.path.join(root_path, label)
+        imgs_list = os.listdir(label_path)
+        print(imgs_list[:10])
+        random.shuffle(imgs_list)
+        output_list[label] = imgs_list[:need_amount]
+        print(output_list[label][:10])
+
+    return output_list
+
+if __name__ == "__main__":
+    root_dir = r"D:\A_myData\RC26-Vision\dataset\juanZhou_cls_real1"
+    output_dir = r"D:\A_myData\RC26-Vision\dataset\juanZhou_cls_real2"
+    os.makedirs(output_dir, exist_ok=True)
+
+    copy_dirs(root_dir, output_dir)
+
+    label_copy_dict = balance_amount(root_dir)
+
+    for label in label_copy_dict:
+        old_path = os.path.join(root_dir, label)
+        new_path = os.path.join(output_dir, label)
+        for img_name in label_copy_dict[label]:
+            old_img_path = os.path.join(old_path, img_name)
+            new_img_path = os.path.join(new_path, img_name)
+            shutil.copy(old_img_path, new_img_path)
